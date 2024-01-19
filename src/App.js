19f584fc3fa297cd8b27d66c200881c7906ecb7a
seoyeon-1206 <ssy_1206@naver.com>
2024-01-19 20:41:30 +0900

    [Feat] todo 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState([
+    {id: 0, title: "", body: "", isDone: false}])
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const nameChangeHandler = (event) =>{
+    setTitle(event.target.value)
+  }
+
+  const bodyChangeHandler = (event) =>{
+    setBody(event.target.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    const newTodo = {
+      id: todo.length + 1,
+      title,
+      body,
+      isDone: false
+    }
+    setTodo([...todo, newTodo]);
+    setTitle("")
+    setBody("");
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-style'>
+       <div>
+   My Todo List
     </div>
+    <div>
+      제목 :&nbsp;
+      <input
+      value ={title}
+      onChange={(event)=>nameChangeHandler(event)} //인자를 넣어줄때는 이렇게 함수로 감싸줌
+      />
+      
+      내용 :&nbsp;
+      <input
+      value = {body}
+      onChange={bodyChangeHandler}
+      />
+      
+      <button onClick={onSubmitHandler}>추가하기</button>
+    </div>
+    <div>
+      <h3>Working</h3>
+      {todo.map((item) => {
+        return(
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.body}</p>
+          </div>
+        ) //반드시 태그 붙이기(key) 그리고 중괄호 안에 리턴()
+    })}
+      
+    </div>
+    <div>
+      <h3>Done</h3>
+    </div>
+    </div>
+
   );
 }
 
