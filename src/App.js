b7ead9e306d1c3a610f2450026a8a85380e94635
seoyeon-1206 <ssy_1206@naver.com>
2024-01-19 22:10:18 +0900

    [Feat] todo 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todo, setTodo] = useState([
-    {id: 0, title: "", body: "", isDone: false}])
+  const [todo, setTodo] = useState([])
 
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -28,7 +27,10 @@ function App() {
     setBody("");
   }
 
-
+  const removeButtonHandler = (id) => {
+    const filteredTodo = todo.filter((todo) => todo.id != id);
+    setTodo(filteredTodo);
+  }
 
   return (
     <div className='app-style'>
@@ -41,27 +43,30 @@ function App() {
       value ={title}
       onChange={(event)=>nameChangeHandler(event)} //인자를 넣어줄때는 이렇게 함수로 감싸줌
       />
-      
       내용 :&nbsp;
       <input
       value = {body}
       onChange={bodyChangeHandler}
       />
-      
       <button onClick={onSubmitHandler}>추가하기</button>
     </div>
+
     <div>
       <h3>Working</h3>
+      <div className='working-list'>
       {todo.map((item) => {
         return(
-          <div key={item.id}>
-            <p>{item.title}</p>
+          <div key={item.id} className='todo-style'>
+            <h1>{item.title}</h1>
             <p>{item.body}</p>
+            <button onClick={() => removeButtonHandler(item.id)}>삭제</button>
+            <button>완료</button>
           </div>
         ) //반드시 태그 붙이기(key) 그리고 중괄호 안에 리턴()
     })}
-      
+      </div>
     </div>
+
     <div>
       <h3>Done</h3>
     </div>
