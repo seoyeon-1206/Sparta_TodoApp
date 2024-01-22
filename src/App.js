67d5c9be410199a6c7a1619c676c3e41f0a9dc0d
seoyeon-1206 <ssy_1206@naver.com>
2024-01-22 19:12:03 +0900

    [Feat] TodoItem 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import TodoItem from './component/TodoItem';
 
 function App() {
   const [todo, setTodo] = useState([])
@@ -7,22 +8,22 @@ function App() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
-  const nameChangeHandler = (event) =>{
+  const nameChangeHandler = (event) => {
     setTitle(event.target.value)
   }
 
-  const bodyChangeHandler = (event) =>{
+  const bodyChangeHandler = (event) => {
     setBody(event.target.value)
   }
 
   const onSubmitHandler = () => {
     const newTodo = {
-      id: Date.now(),  
+      id: Date.now(),
       title,
       body,
       isDone: false
     }
-    if(title === '' || body === '') alert("내용을 추가하세요!");
+    if (title === '' || body === '') alert("내용을 추가하세요!");
     else setTodo([...todo, newTodo]);
     setTitle("")
     setBody("");
@@ -34,9 +35,9 @@ function App() {
   }
 
   const todoUpdateButtonHandler = (idToUpdate) => {
-    const updatedTodo = todo.map((item) =>{
-      if(item.id === idToUpdate){
-        return {...item, isDone : !item.isDone};
+    const updatedTodo = todo.map((item) => {
+      if (item.id === idToUpdate) {
+        return { ...item, isDone: !item.isDone };
       }
       return item;
     })
@@ -45,51 +46,47 @@ function App() {
 
   return (
     <div className='app-style'>
-       <div>
-   My Todo List
-    </div>
-    <div>
-      제목 :&nbsp;
-      <input
-      value ={title}
-      onChange={nameChangeHandler} 
-      />
-      내용 :&nbsp;
-      <input
-      value = {body}
-      onChange={bodyChangeHandler}
-      />
-      <button onClick={onSubmitHandler}>추가하기</button>
-    </div>
-
-    <div>
-      <h3>Working</h3>
-      <div className='working-list'>
-      {todo.filter((item)=> !item.isDone).map((item) => {
-        return(
-          <div key={item.id} className='todo-style'>
-            <h1>{item.title}</h1>
-            <p>{item.body}</p>
-            <button onClick={() => removeButtonHandler(item.id)}>삭제</button>
-            <button onClick={() => todoUpdateButtonHandler(item.id)}>완료</button>
-          </div>
-        ) 
-    })}
+      <div>
+        My Todo List
       </div>
-    </div>
+      <div>
+        제목 :&nbsp;
+        <input
+          value={title}
+          onChange={nameChangeHandler}
+        />
+        내용 :&nbsp;
+        <input
+          value={body}
+          onChange={bodyChangeHandler}
+        />
+        <button onClick={onSubmitHandler}>추가하기</button>
+      </div>
 
-    <div>
-      <h3>Done</h3>
-      {todo.filter((item)=> item.isDone).map((item) => {
-        return(
-          <div key={item.id} className='todo-style'>
-            <h1>{item.title}</h1>
-            <p>{item.body}</p>
-            <button onClick={() => removeButtonHandler(item.id)}>삭제</button>
-            <button onClick={() => todoUpdateButtonHandler(item.id)}>{item.isDone ? "취소" : "완료"}</button>
-          </div>
-        ) 
-    })}
+      <div>
+        <h3>Working</h3>
+        <div className='working-list'>
+          {todo.filter((item) => !item.isDone).map((item) =>
+            <TodoItem
+              key={item.id}
+              item={item}
+              onRemove={removeButtonHandler}
+              onUpdate={todoUpdateButtonHandler}
+            />
+          )}
+        </div>
+      </div>
+
+      <div>
+        <h3>Done</h3>
+        {todo.filter((item) => item.isDone).map((item) =>
+          <TodoItem
+            key={item.id}
+            item={item}
+            onRemove={removeButtonHandler}
+            onUpdate={todoUpdateButtonHandler}
+          />
+        )}
       </div>
     </div>
   );
